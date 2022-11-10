04bc3c8302a39a3d10f28bf83ed1146aeaf278bb
NingYang <nyang@yipitdata.com>
2022-11-10 18:24:08 +0800

    GraphQLScalarType

--- src/main/Graphql/schema/types/config.ts
+++ src/main/Graphql/schema/types/config.ts
@@ -1,19 +1,53 @@
-import { GraphQLScalarType } from 'graphql'
+import { GraphQLError, GraphQLScalarType, Kind } from 'graphql'
 
 export const GraphQLConfigValueType = new GraphQLScalarType({
   name: 'ConfigValue',
+  // 将标量的后端表示转换为与 JSON 兼容的格式, 以便 Apollo Server 可以将其包含在操作响应中
   serialize(value) {
-    if (
-      typeof value === 'boolean' ||
-      typeof value === 'number' ||
-      typeof value === 'string' ||
-      value instanceof Date
-    ) {
+    if (typeof value === 'boolean' || typeof value === 'number' || typeof value === 'string') {
       return value
+    } else if (value instanceof Date) {
+      return value.getTime()
     } else {
       throw new TypeError(
         `Value is not an instance of Date or boolean or number or string: ${value}`
       )
     }
+  },
+  // 解析参数: 当标量作为变量传入时, 将标量的 JSON 值解析成后端表示值并添加到解析器的args
+  parseValue(value) {
+    // Date 类型的 value 将会是类似字符串: "2022-11-10T09:55:44.092Z"
+    if (typeof value === 'boolean' || typeof value === 'number') {
+      return value
+    }
+    if (typeof value != 'string') {
+      throw new TypeError(
+        `Value is not an instance of Date or boolean or number or string: ${value}`
+      )
+    }
+    if (Number.isNaN(Number(value)) && !Number.isNaN(Date.parse(value))) {
+      return new Date(Date.parse(value))
+    }
+    return value
+  },
+  // 解析参数: 当标量不是作为变量传入, 而是硬编码在请求语句中, 将标量的 JSON 值解析成后端表示值并添加到解析器的args
+  parseLiteral(valueAST) {
+    if (!('value' in valueAST)) {
+      throw new GraphQLError(`Value is not valid, kind is ${valueAST.kind}`)
+    }
+    if (
+      valueAST.kind === Kind.INT ||
+      valueAST.kind === Kind.FLOAT ||
+      valueAST.kind === Kind.BOOLEAN
+    ) {
+      return valueAST.value
+    }
+    if (valueAST.kind != Kind.STRING) {
+      throw new GraphQLError(`Value is not valid: ${valueAST.value}`)
+    }
+    if (Number.isNaN(Number(valueAST.value)) && !Number.isNaN(Date.parse(valueAST.value))) {
+      return new Date(Date.parse(valueAST.value))
+    }
+    return valueAST.value
   }
 })
--- src/main/Graphql/schema/types/date.ts
+++ src/main/Graphql/schema/types/date.ts
@@ -1,30 +1,40 @@
 import { GraphQLError, GraphQLScalarType, Kind } from 'graphql'
 
-export const GraphQLDate = new GraphQLScalarType({
+export const GraphQLDate = new GraphQLScalarType<Date, number>({
   name: 'Date',
   description: 'Date custom scalar type',
+  // 将标量的后端表示转换为与 JSON 兼容的格式, 以便 Apollo Server 可以将其包含在操作响应中
   serialize(value) {
     if (value instanceof Date) {
-      return value
+      return value.getTime()
     } else if (typeof value === 'number') {
-      return new Date(value)
+      return value
     } else {
       throw new TypeError(`Value is not an instance of Date or number: ${value}`)
     }
   },
+  // 解析参数: 当标量作为变量传入时, 将标量的 JSON 值解析成后端表示值并添加到解析器的args
   parseValue(value) {
+    // value 将会是类似字符串: "2022-11-10T09:55:44.092Z"
     const date = new Date(value as string)
     if (Number.isNaN(date.getTime())) {
       throw new TypeError(`Value is not a valid Date: ${value}`)
     }
     return date
   },
+  // 解析参数: 当标量不是作为变量传入, 而是硬编码在请求语句中, 将标量的 JSON 值解析成后端表示值并添加到解析器的args
   parseLiteral(valueAST) {
-    if (valueAST.kind !== Kind.INT && valueAST.kind !== Kind.FLOAT) {
-      throw new GraphQLError(`Can only parse numbers but got a: ${valueAST.kind}`)
+    if (!('value' in valueAST)) {
+      throw new GraphQLError(`Value is not a valid Date, kind is ${valueAST.kind}`)
     }
-    const result = new Date(Number(valueAST.value))
-    if (Number.isNaN(result.getTime())) {
+    let result: Date | null = null
+    if (valueAST.kind === Kind.INT || valueAST.kind === Kind.FLOAT) {
+      result = new Date(Number(valueAST.value))
+    }
+    if (valueAST.kind === Kind.STRING) {
+      result = new Date(valueAST.value as string)
+    }
+    if (!result || Number.isNaN(result.getTime())) {
       throw new GraphQLError(`Value is not a valid Date: ${valueAST.value}`)
     }
     return result

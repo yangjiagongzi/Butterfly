9f593975e9eaed60afbcdd774a24388494a683fc
NingYang <nyang@yipitdata.com>
2023-04-21 16:57:46 +0800

    only set dock icon in dev

--- src/main/Application/index.ts
+++ src/main/Application/index.ts
@@ -1,13 +1,17 @@
 import { app } from 'electron'
 import path from 'path'
+import AppConfig from '~/config'
 import BrowserWindowManagement from '../BrowserWindowManagement'
 import Config from '../Config'
 import GraphqlServer from '../Graphql'
 import IPCListener from '../IPCListener'
+import { EnvEnum } from '~/config/constant'
 
 class Application {
   start = () => {
-    app.dock.setIcon(path.join(process.cwd(), 'src/assets/icon/icon_circle.png'))
+    if (AppConfig.currentEnv() === EnvEnum.dev) {
+      app.dock.setIcon(path.join(process.cwd(), 'src/assets/icon/icon_circle.png'))
+    }
     IPCListener.registListener()
     GraphqlServer.startApolloServer()
     Config.initConfig()
